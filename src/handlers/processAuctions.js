abda157a5761b00f3509b7d4124bf5e42d2877f3
Jiwoo Choi <jchoi136@gmail.com>
2022-09-21 21:34:17 -0700

    added processing auction operations

---
+++ src/handlers/processAuctions.js
@@ -0,0 +1,19 @@
+import createError from "http-errors";
+import { closeAuction } from "../lib/closeAuction";
+import { getEndedAuctions } from "../lib/getEndedAuctions";
+
+async function processAuctions(event, context) {
+  try {
+    const auctionToClose = await getEndedAuctions();
+    const closePromises = auctionToClose.map((auction) =>
+      closeAuction(auction)
+    );
+    await Promise.all(closePromises);
+    return { closed: closePromises.length };
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+}
+
+export const handler = processAuctions;
